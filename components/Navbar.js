215a342d809c19217cbd85e16732962a50d58896
NightRaven-spec <73557051+NightRaven-spec@users.noreply.github.com>
2022-04-07 19:26:39 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -73,7 +73,7 @@ export default function NavBar() {
                   zIndex: -1,
                 }}
               >
-                <NextLink href="/">🤝BetterFund</NextLink>
+                <NextLink href="/">�OnTheBlockFund</NextLink>
               </Box>
             </Heading>
           </Flex>
